d67d499051d85ffdb9ff1210a4ecfc7e71dfac59
Benny <bennyv314@duck.com>
2026-03-19 10:55:17 +0200

    feat: add interactive visualization and rasterization

--- MyPTV/myptv/masking_mod.py
+++ MyPTV/myptv/masking_mod.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.spatial import ConvexHull
+import matplotlib.pyplot as plt
+from skimage import draw, io
 
 def load_blobs(fname):
     data = np.loadtxt(fname)
@@ -39,3 +41,32 @@ def get_padded_hull(points, padding):
         new_vertices.append(v_curr + m_curr * (padding / cos_theta))
         
     return np.array(new_vertices)
+
+def generate_blob_polygon_mask(blob_file, resolution, padding, output_bit_depth, save_name):
+    points = load_blobs(blob_file)
+    padded_hull = get_padded_hull(points, padding)
+    
+    # Clip padded hull to resolution [width, height]
+    # Note: resolution param is [width, height]
+    padded_hull[:, 0] = np.clip(padded_hull[:, 0], 0, resolution[1]-1) # Y
+    padded_hull[:, 1] = np.clip(padded_hull[:, 1], 0, resolution[0]-1) # X
+    
+    fig, ax = plt.subplots()
+    ax.scatter(points[:, 1], points[:, 0], c='blue', label='Blobs', s=5)
+    ax.plot(np.append(padded_hull[:, 1], padded_hull[0, 1]), 
+            np.append(padded_hull[:, 0], padded_hull[0, 0]), 'r-', label='Padded Hull')
+    ax.set_xlim(0, resolution[0])
+    ax.set_ylim(resolution[1], 0) # Flip Y for image coords
+    ax.legend()
+    ax.set_title("Approve Polygon Mask? Close to save, Ctrl+C in terminal to abort.")
+    plt.show()
+    
+    # Rasterize
+    mask = np.zeros((resolution[1], resolution[0]), dtype=np.uint8 if output_bit_depth == 8 else np.uint16)
+    rr, cc = draw.polygon(padded_hull[:, 0], padded_hull[:, 1], mask.shape)
+    
+    val = 255 if output_bit_depth == 8 else 65535
+    mask[rr, cc] = val
+    
+    io.imsave(save_name, mask, check_contrast=False)
+    print(f"Mask saved to {save_name}")
