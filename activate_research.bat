db964a95f5decddd23c0404ef977422a94b2c5b6
bennyV13 <bennyv314@duck.com>
2026-04-22 11:50:23 +0300

    Add Windows CMD activation script

---
+++ activate_research.bat
@@ -0,0 +1,5 @@
+@echo off
+call "C:\Users\bennyv\.venvs\research\Scripts\activate.bat"
+set PYTHONPATH=
+echo Research environment activated (CMD).
+echo PYTHONPATH has been cleared to avoid conflicts.
